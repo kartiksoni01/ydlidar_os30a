eec98ffaf09b2427d3fe3cc20ad853b4a95472cd
kartikPC <kartiksoni010@gmail.com>
2024-06-06 18:23:37 +0530

    Frame changes

--- bimu_rgbd_camera/src/bimu_rgbd_camera.cpp
+++ bimu_rgbd_camera/src/bimu_rgbd_camera.cpp
@@ -1,11 +1,12 @@
 #include "ros/ros.h"
 #include "ros/time.h"
 #include "std_msgs/String.h"
-
 #include <sstream>
-
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/Image.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <stdlib.h>
 
 typedef struct 
 {
@@ -13,17 +14,47 @@ typedef struct
     int cols = 640;
 } picture;
 
+void publishStaticTransforms(tf2_ros::StaticTransformBroadcaster& static_broadcaster) {
+    geometry_msgs::TransformStamped transformStamped;
+
+    // Transform for RGB image frame
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "camera_link";
+    transformStamped.child_frame_id = "camera_color_optical_frame";
+    transformStamped.transform.translation.x = 0.0;
+    transformStamped.transform.translation.y = 0.0;
+    transformStamped.transform.translation.z = 0.0;
+    transformStamped.transform.rotation.x = 0.0;
+    transformStamped.transform.rotation.y = 0.0;
+    transformStamped.transform.rotation.z = 0.0;
+    transformStamped.transform.rotation.w = 1.0;
+    static_broadcaster.sendTransform(transformStamped);
+
+    // Transform for Depth image frame
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "camera_link";
+    transformStamped.child_frame_id = "camera_depth_optical_frame";
+    transformStamped.transform.translation.x = 0.0;
+    transformStamped.transform.translation.y = 0.0;
+    transformStamped.transform.translation.z = 0.0;
+    transformStamped.transform.rotation.x = 0.0;
+    transformStamped.transform.rotation.y = 0.0;
+    transformStamped.transform.rotation.z = 0.0;
+    transformStamped.transform.rotation.w = 1.0;
+    static_broadcaster.sendTransform(transformStamped);
+}
+
 int main(int argc, char **argv)
 {
-
     ros::init(argc, argv, "bimu_rgbd_camera");
-
     ros::NodeHandle n;
-    ros::Publisher img_pub = n.advertise<sensor_msgs::Image>("bimu_rgbd_camera", 1000);
-    ros::Publisher depth_pub = n.advertise<sensor_msgs::Image>("bimu_depth_camera", 1000);
+    ros::Publisher img_pub = n.advertise<sensor_msgs::Image>("camera/color/image_raw", 1000);
+    ros::Publisher depth_pub = n.advertise<sensor_msgs::Image>("camera/depth/image_raw", 1000);
+
+    tf2_ros::StaticTransformBroadcaster static_broadcaster;
+    publishStaticTransforms(static_broadcaster);
 
     ros::Rate loop_rate(10);
-
     int count = 0;
 
     sensor_msgs::Image ros_image;
@@ -35,23 +66,23 @@ int main(int argc, char **argv)
     ROS_INFO("%d, %d", picture_data.rows, picture_data.cols);
 
     RGBbuffer = (unsigned char *)malloc(picture_data.rows * picture_data.cols * 3 * sizeof(unsigned char));
-    for(int v=0; v<picture_data.rows; v++)
-        for(int u=0; u<picture_data.cols; u++){
-            RGBbuffer[3*(v*picture_data.cols+u)] = v%255;
-            RGBbuffer[3*(v*picture_data.cols+u)+1] = u%255;
-            RGBbuffer[3*(v*picture_data.cols+u)+2] = 0;
+    for(int v = 0; v < picture_data.rows; v++)
+        for(int u = 0; u < picture_data.cols; u++){
+            RGBbuffer[3 * (v * picture_data.cols + u)] = v % 255;
+            RGBbuffer[3 * (v * picture_data.cols + u) + 1] = u % 255;
+            RGBbuffer[3 * (v * picture_data.cols + u) + 2] = 0;
         }
 
     DEPTHbuffer = (unsigned short *)malloc(depth_data.rows * depth_data.cols * sizeof(unsigned short));
-    for(int v=0; v<depth_data.rows; v++)
-        for(int u=0; u<depth_data.cols; u++){
-            DEPTHbuffer[v*depth_data.cols+u] = v;
+    for(int v = 0; v < depth_data.rows; v++)
+        for(int u = 0; u < depth_data.cols; u++){
+            DEPTHbuffer[v * depth_data.cols + u] = v;
         }
 
     while (ros::ok())
     {
         ros_image.header.stamp = ros::Time::now();
-        ros_image.header.frame_id = "bimu_rgbd_camera_frame"; // Set the camera frame
+        ros_image.header.frame_id = "camera_color_optical_frame"; // Set the camera frame
         ros_image.height = picture_data.rows;
         ros_image.width = picture_data.cols;
         ros_image.encoding = "rgb8";
@@ -63,7 +94,7 @@ int main(int argc, char **argv)
         memcpy((char*)(&ros_image.data[0]), RGBbuffer, size);
 
         depth_image.header.stamp = ros::Time::now();
-        depth_image.header.frame_id = "bimu_depth_camera_frame"; // Set the depth camera frame
+        depth_image.header.frame_id = "camera_depth_optical_frame"; // Set the depth camera frame
         depth_image.height = depth_data.rows;
         depth_image.width = depth_data.cols;
         depth_image.encoding = "mono16";
@@ -81,6 +112,9 @@ int main(int argc, char **argv)
         loop_rate.sleep();
         ++count;
     }
+
+    free(RGBbuffer);
+    free(DEPTHbuffer);
 
     return 0;
 }
